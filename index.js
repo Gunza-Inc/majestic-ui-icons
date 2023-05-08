20010ec9afc3e2b2cfb15045de4c21f1e2f0c40b
loremru <mr.nonamerz08@yandex.ru>
2023-05-08 11:42:00 +0300

    fix: export

---
+++ index.js
@@ -0,0 +1,44 @@
+import AppGallery from './icons/app-gallery.svg'
+import AppStore from './icons/app-store.svg'
+import AppStoreInvert from './icons/app-store-invert.svg'
+import Dzen from './icons/dzen.svg'
+import DzenInvert from './icons/dzen-invert.svg'
+import Gunza from './icons/gunza.svg'
+import Inst from './icons/instagram.svg'
+import Now from './icons/now.svg'
+import NowInvert from './icons/now-invert.svg'
+import Ok from './icons/ok.svg'
+import PlayMarket from './icons/play-market.svg'
+import PlayMarketInvert from './icons/play-market-invert.svg'
+import Rustore from './icons/rustore.svg'
+import Rutube from './icons/rutube.svg'
+import RutubeInvert from './icons/rutube-invert.svg'
+import Telegram from './icons/telegram.svg'
+import Tiktok from './icons/tiktok.svg'
+import TiktokInvert from './icons/tiktok-invert.svg'
+import Vk from './icons/vk.svg'
+import Youtube from './icons/youtube.svg'
+import YoutubeInvert from './icons/youtube-invert.svg'
+import YoutubeAlt from './icons/youtube-alt.svg'
+import YoutubeAltInvert from './icons/youtube-alt-invert.svg'
+import YoutubeShortsInvert from './icons/youtube-shorts-invert.svg'
+import YoutubeShorts from './icons/youtube-shorts.svg'
+
+export default {
+	'app-gallery': [AppGallery],
+	'app-store': [AppStore, AppStoreInvert],
+	'dzen': [Dzen, DzenInvert],
+	'gunza': [Gunza],
+	'instagram': [Inst],
+	'now': [Now, NowInvert],
+	'ok': [Ok],
+	'play-market': [PlayMarket, PlayMarketInvert],
+	'rustore': [Rustore],
+	'rutube': [Rutube, RutubeInvert],
+	'telegram': [Telegram],
+	'tiktok': [Tiktok, TiktokInvert],
+	'vk': [Vk],
+	'youtube': [Youtube, YoutubeInvert],
+	'youtube-alt': [YoutubeAlt, YoutubeAltInvert],
+	'youtube-shorts': [YoutubeShorts, YoutubeShortsInvert],
+}
