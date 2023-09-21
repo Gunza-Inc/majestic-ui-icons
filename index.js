a87bf4486e84959377419781920aa283d13fc5d2
loremru <mr.nonamerz08@yandex.ru>
2023-09-21 19:40:24 +0300

    added shapes for avatars

--- index.js
+++ index.js
@@ -24,6 +24,67 @@ import YoutubeAltInvert from './icons/youtube-alt-invert.svg'
 import YoutubeShortsInvert from './icons/youtube-shorts-invert.svg'
 import YoutubeShorts from './icons/youtube-shorts.svg'
 
+import Shape0 from './shapes/Shape-0.svg';
+import Shape1 from './shapes/Shape-1.svg';
+import Shape2 from './shapes/Shape-2.svg';
+import Shape3 from './shapes/Shape-3.svg';
+import Shape4 from './shapes/Shape-4.svg';
+import Shape5 from './shapes/Shape-5.svg';
+import Shape6 from './shapes/Shape-6.svg';
+import Shape7 from './shapes/Shape-7.svg';
+import Shape8 from './shapes/Shape-8.svg';
+import Shape9 from './shapes/Shape-9.svg';
+import Shape10 from './shapes/Shape-10.svg';
+import Shape11 from './shapes/Shape-11.svg';
+import Shape12 from './shapes/Shape-12.svg';
+import Shape13 from './shapes/Shape-13.svg';
+import Shape14 from './shapes/Shape-14.svg';
+import Shape15 from './shapes/Shape-15.svg';
+import Shape16 from './shapes/Shape-16.svg';
+import Shape17 from './shapes/Shape-17.svg';
+import Shape18 from './shapes/Shape-18.svg';
+import Shape19 from './shapes/Shape-19.svg';
+import Shape20 from './shapes/Shape-20.svg';
+import Shape21 from './shapes/Shape-21.svg';
+import Shape22 from './shapes/Shape-22.svg';
+import Shape23 from './shapes/Shape-23.svg';
+import Shape24 from './shapes/Shape-24.svg';
+import Shape25 from './shapes/Shape-25.svg';
+import Shape26 from './shapes/Shape-26.svg';
+import Shape27 from './shapes/Shape-27.svg';
+import Shape28 from './shapes/Shape-28.svg';
+import Shape29 from './shapes/Shape-29.svg';
+import Shape30 from './shapes/Shape-30.svg';
+import Shape31 from './shapes/Shape-31.svg';
+import Shape32 from './shapes/Shape-32.svg';
+import Shape33 from './shapes/Shape-33.svg';
+import Shape34 from './shapes/Shape-34.svg';
+import Shape35 from './shapes/Shape-35.svg';
+import Shape36 from './shapes/Shape-36.svg';
+import Shape37 from './shapes/Shape-37.svg';
+import Shape38 from './shapes/Shape-38.svg';
+import Shape39 from './shapes/Shape-39.svg';
+import Shape40 from './shapes/Shape-40.svg';
+import Shape41 from './shapes/Shape-41.svg';
+import Shape42 from './shapes/Shape-42.svg';
+import Shape43 from './shapes/Shape-43.svg';
+import Shape44 from './shapes/Shape-44.svg';
+import Shape45 from './shapes/Shape-45.svg';
+import Shape46 from './shapes/Shape-46.svg';
+import Shape47 from './shapes/Shape-47.svg';
+import Shape48 from './shapes/Shape-48.svg';
+import Shape49 from './shapes/Shape-49.svg';
+import Shape50 from './shapes/Shape-50.svg';
+import Shape51 from './shapes/Shape-51.svg';
+import Shape52 from './shapes/Shape-52.svg';
+import Shape53 from './shapes/Shape-53.svg';
+import Shape54 from './shapes/Shape-54.svg';
+import Shape55 from './shapes/Shape-55.svg';
+import Shape56 from './shapes/Shape-56.svg';
+import Shape57 from './shapes/Shape-57.svg';
+import Shape58 from './shapes/Shape-58.svg';
+import Shape59 from './shapes/Shape-59.svg';
+
 export default {
 	'app-gallery': [AppGallery],
 	'app-store': [AppStore, AppStoreInvert],
@@ -41,4 +102,66 @@ export default {
 	'youtube': [Youtube, YoutubeInvert],
 	'youtube-alt': [YoutubeAlt, YoutubeAltInvert],
 	'youtube-shorts': [YoutubeShorts, YoutubeShortsInvert],
+	shapes: {
+		'0': Shape0,
+		'1': Shape1,
+		'2': Shape2,
+		'3': Shape3,
+		'4': Shape4,
+		'5': Shape5,
+		'6': Shape6,
+		'7': Shape7,
+		'8': Shape8,
+		'9': Shape9,
+		'10': Shape10,
+		'11': Shape11,
+		'12': Shape12,
+		'13': Shape13,
+		'14': Shape14,
+		'15': Shape15,
+		'16': Shape16,
+		'17': Shape17,
+		'18': Shape18,
+		'19': Shape19,
+		'20': Shape20,
+		'21': Shape21,
+		'22': Shape22,
+		'23': Shape23,
+		'24': Shape24,
+		'25': Shape25,
+		'26': Shape26,
+		'27': Shape27,
+		'28': Shape28,
+		'29': Shape29,
+		'30': Shape30,
+		'31': Shape31,
+		'32': Shape32,
+		'33': Shape33,
+		'34': Shape34,
+		'35': Shape35,
+		'36': Shape36,
+		'37': Shape37,
+		'38': Shape38,
+		'39': Shape39,
+		'40': Shape40,
+		'41': Shape41,
+		'42': Shape42,
+		'43': Shape43,
+		'44': Shape44,
+		'45': Shape45,
+		'46': Shape46,
+		'47': Shape47,
+		'48': Shape48,
+		'49': Shape49,
+		'50': Shape50,
+		'51': Shape51,
+		'52': Shape52,
+		'53': Shape53,
+		'54': Shape54,
+		'55': Shape55,
+		'56': Shape56,
+		'57': Shape57,
+		'58': Shape58,
+		'59': Shape59,
+	}
 }
